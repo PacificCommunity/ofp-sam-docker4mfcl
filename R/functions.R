f01499e6ec69d9a7494431572e9162b770d7633d
kyuhank <kh2064@gmail.com>
2025-01-16 13:56:37 +0900

    docker pull and push

--- R/functions.R
+++ R/functions.R
@@ -385,3 +385,59 @@ CMD [\"make\"]",
 }
 
 
+#' Pull a Docker image from Docker Hub
+#' 
+#' This function pulls a Docker image from Docker Hub 
+#' 
+#' @param image_name The name of the Docker image.
+#' @param version The version of the Docker image. Defaults to "latest".
+#' @examples
+#' docker_pull("my_image", "1.0")
+docker_pull <- function(image_name, version = "latest") {
+  # Combine image name and version
+  full_image_name <- sprintf("%s:%s", image_name, version)
+  
+  # Check if the image already exists locally
+  image_exists <- function(image_name) {
+    command <- sprintf("docker images -q %s", image_name)
+    result <- system(command, intern = TRUE)
+    return(length(result) > 0)
+  }
+  
+  # Pull the image if it doesn't exist locally
+  if (!image_exists(full_image_name)) {
+    cat(sprintf("Image '%s' not found locally. Pulling from Docker Hub...\n", full_image_name))
+    pull_command <- sprintf("docker pull %s", full_image_name)
+    result <- system(pull_command, intern = TRUE)
+    cat(result, sep = "\n")
+  } else {
+    cat(sprintf("Image '%s' already exists locally. No need to pull.\n", full_image_name))
+  }
+}
+
+
+
+#' Push a Docker image to Docker Hub
+#' 
+#' This function pushes a Docker image to Docker Hub
+#' 
+#' @param local_image The name of the local Docker image.
+#' @param remote_image The name of the remote Docker image.
+#' @param username The Docker Hub username.
+#' @param tag The tag of the Docker image. Defaults to "latest".
+#' @examples
+docker_push <- function(local_image, remote_image, username, tag = "latest") {
+  # Combine image name and tag
+  full_local_image <- sprintf("%s:%s", local_image, tag)
+  full_remote_image <- sprintf("%s/%s:%s", username, remote_image, tag)
+  
+  # Tag the image for Docker Hub
+  cat(sprintf("Tagging image: %s as %s\n", full_local_image, full_remote_image))
+  system(sprintf("docker tag %s %s", full_local_image, full_remote_image))
+  
+  # Push the image to Docker Hub
+  cat(sprintf("Pushing image: %s to Docker Hub\n", full_remote_image))
+  system(sprintf("docker push %s", full_remote_image))
+}
+
+
